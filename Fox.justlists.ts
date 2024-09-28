4d7200adca88440895c9a852a069f8504517ead3
sheilasoft988 <sheilasoft988@gmail.com>
2024-09-28 23:56:09 +0800

    Update Fox.justlists.ts

--- Fox.justlists.ts
+++ Fox.justlists.ts
@@ -2,13 +2,29 @@
     "name":"Fox",
     "tokens":[
         {
+            "symbol":"WTRX",
+            "address":"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+            "chainId":1,
+            "decimals":6,
+            "name":"Wrapped TRX",
+            "logoURI":"https://coin.top/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
+        },
+        {
+            "symbol":"USDT",
+            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            "chainId":1,
+            "decimals":6,
+            "name":"Tether USD",
+            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
+        },
+        {
             "symbol":"FOX",
             "address":"TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3",
             "chainId":1,
             "decimals":6,
             "name":"FoxCoin",
             "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png"
-        } 
+        }
     ],
     "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png",
     "version":{
