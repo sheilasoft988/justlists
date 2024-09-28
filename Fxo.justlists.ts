74e173aa45c03881d4897e12a463d00b37750e3a
sheilasoft988 <sheilasoft988@gmail.com>
2024-09-28 23:40:37 +0800

    Create Fxo.justlists.ts
    test

---
+++ Fxo.justlists.ts
@@ -0,0 +1,29 @@
+{
+    "name":"Fox",
+    "tokens":[
+        {
+            "symbol":"FOX",
+            "address":"TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3",
+            "chainId":1,
+            "decimals":6,
+            "name":"SUN TOKEN",
+            "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png"
+        },
+        {
+            "symbol":"FOX",
+            "address":"TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3",
+            "chainId":1,
+            "decimals":6,
+            "name":"SUN TOKEN",
+            "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png"
+        }
+ 
+    ],
+    "logoURI":"https://justswap.io/favicon.ico",
+    "version":{
+        "patch":0,
+        "major":1,
+        "minor":0
+    },
+    "timestamp":1611147166000
+}
